0603cc5f244f0565cf3b405ae565cb52cebd1763
Grace Stempel <gracestempel@Katara.local>
2016-11-21 15:36:41 -0500

    update files

--- shell.c
+++ shell.c
@@ -1,5 +1,14 @@
 #include "shell.h"
 
+char ** split(char * str, char * delim);
+
+char ** getInput();
+
+void cd(char * path);
+
+void exit();
+
 int main(int argc, char * argv[]) {
+
   return 0;
 }
