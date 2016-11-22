1ea8ec63ae154a74d6ec373dc0f00b7768701def
eVookelj <evukelj@stuy.edu>
2016-11-22 17:58:26 -0500

    Turn rets to heap mem

--- shell.c
+++ shell.c
@@ -7,7 +7,9 @@ char * getInput() {
   printf("%s: ", cwd);
   fgets(input, sizeof(input), stdin);
   *(strchr(input, '\n')) = NULL;
-  return input;
+  char* retInput = (char*) malloc(sizeof(input));
+  retInput = &input[0];
+  return retInput;
 }
 
 void cd(char * path);
@@ -15,17 +17,19 @@ void cd(char * path);
 void exit();
 
 char ** split(char * str, char * delim) {
-  str[strlen(str) - 1] = 0;
   char *command[100];
   int i = 0;
   while((command[i] = strsep(&str, delim)))
     i++;
-  return command;
+  //return command;
+  char** cmd = (char**) malloc(sizeof(command));
+  cmd = &command[0];
+  return cmd;
 }
 
 int main(int argc, char * argv[]) {
   char *in = getInput();
-  printf("\nInput: %s",in);
+  printf("\nInput: %s\n",in);
   char **in2 = split(in, " ");
   while(*in2) {
     printf("\nInput after split: %s",*in2);
