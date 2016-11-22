cccf50b42ae7d4511fb129eb9d4bdf50e067a2a8
eVookelj <evukelj@stuy.edu>
2016-11-21 20:23:45 -0500

    Reset everything because coding is hard!

--- shell.c
+++ shell.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+#include <string.h>
 
 char ** getInput();
 
@@ -7,17 +8,7 @@ void cd(char * path);
 void exit();
 
 char ** split(char * str, char * delim) {
-  char **retVal = (char **)malloc(sizeof(str) * sizeof(char *));
-  int i = 0;
-  while (str) {
-    retVal[i] = strsep(&str, delim);
-    i++;
-  }
-  retVal[i] = 0;
-  return retVal;
 }
 
 int main(int argc, char * argv[]) {
-  printf("val: %s\n",split("Emma B Vukelj", " ")[0]);
-  return 0;
 }
