799d05b8d936720f23b623c47d8af560b3f557b9
Grace Stempel <gracestempel@Katara.local>
2016-11-22 13:23:00 -0500

    do split

--- shell.c
+++ shell.c
@@ -14,8 +14,24 @@ void cd(char * path);
 void exit();
 
 char ** split(char * str, char * delim) {
+  str[strlen(str) - 1] = 0;
+  char *command[100];
+  int i = 0;
+  while(str) {
+    command[i] = strsep(&str, delim);
+    i++;
+  }
+  command[i] = 0;
+  return command;
 }
 
 int main(int argc, char * argv[]) {
-  printf("\nInput: %s",getInput());
+  char *in = getInput();
+  printf("\nInput: %s",in);
+  char **in2 = split(in, " ");
+  while(*in2) {
+    printf("\nInput after split: %s",*in2);
+    in2 ++;
+  }
+  printf("\n");
 }
