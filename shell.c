4c6c6288695d6241c28e68c4af38b0037e946e48
eVookelj <evukelj@stuy.edu>
2016-11-28 15:04:33 -0500

    Revert "Mess around (not much progress)"
    This reverts commit 80f30b6e89a9a863c3525db752f24b4ab9ed59b0.
    
    revert

--- shell.c
+++ shell.c
@@ -33,8 +33,8 @@ char ** split(char * str, char * delim) {
   command[i] = 0;
   
   //turn heap mem to more persistent mem for return
-  char** cmd = (char**) malloc(sizeof(char*) * 100);
-  //IS ISSUE HERE
+  char** cmd = (char**) malloc(sizeof(command));
+  cmd = &(command[0]);
   return cmd;
 }
 
@@ -77,8 +77,7 @@ void run() {
       char** cmdSplit = split(cmd," ");
       printf("AFTER: %s\n", cmds[i]);
 
-      free(cmdSplit);
-      free(cmd);
+      
       //exec(cmdSplit);
     }
   }
